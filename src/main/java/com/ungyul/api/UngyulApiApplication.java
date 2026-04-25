67a799f052806fd14a46a7901b2767f483003025
kimsolee <ahhasolee95@gmail.com>
2026-04-25 22:39:06 +0900

    feat: init commit

---
+++ src/main/java/com/ungyul/api/UngyulApiApplication.java
@@ -0,0 +1,13 @@
+package com.ungyul.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UngyulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UngyulApiApplication.class, args);
+	}
+
+}
